4f1cf6c22c7500b19dba62a67c82589f43588309
MarioVenturaMedeiros <Mario.Medeiros@sou.inteli.edu.br>
2023-04-05 09:33:03 -0300

    att

--- src/area_elevador.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-#leva ele para a proxima cena ao encostar na area
-func _on_Area2D2_body_entered(_body:Node):
-	get_tree().change_scene("res://cenas_princiapais/batalha_shokinho.tscn")

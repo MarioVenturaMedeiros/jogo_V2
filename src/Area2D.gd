5e52844915d69c4c84217dd2906255ad09ad1e58
MarioVenturaMedeiros <Mario.Medeiros@sou.inteli.edu.br>
2023-04-05 10:17:02 -0300

    att

--- src/Area2D.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-#caso o corpo entre nessa area ele é levado para a proxima cena
-func _on_Area2D_body_entered(body : Node):
-	get_tree().change_scene("res://cenas_princiapais/Cena_corredor.tscn")
